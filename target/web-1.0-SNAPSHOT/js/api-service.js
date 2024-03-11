ec0ad573302b86aa639b9df85253d7609879ecc0
JordanAlemanSantan-IESElrincon <125272829+JordanAlemanSantan-IESElrincon@users.noreply.github.com>
2024-03-11 13:02:29 +0000

    Módulos node-modules y target removidos

--- target/web-1.0-SNAPSHOT/js/api-service.js
+++
@@ -1,5 +0,0 @@
-export function fetchWeeklyConsumption(waterId, lightId) {
-    return fetch(`http://23.97.221.154:8080/api-kiosco/api/measurements/getConsumptionBySensorWeekly/${waterId}/${lightId}`)
-        .then(response => response.json())
-        .catch(error => console.error('Error:', error));
-}
