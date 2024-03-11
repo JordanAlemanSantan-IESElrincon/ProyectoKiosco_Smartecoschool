fbe09de4fc95c5c28574eb5fd1a2910234720d2f
Jordan <jordanmanuelalemansantana@alumno.ieselrincon.es>
2024-03-11 19:29:01 +0000

    Comienzo opción de contenidoConsumo alternativo

--- src/main/webapp/js/controlVistasGráficas.js
+++ src/main/webapp/js/controlVistasGráficas.js
@@ -35,41 +35,57 @@
 //
 // setInterval(cambiarVisibilidad, 8000);  // 8,000 milisegundos = 8 segundos
 
-const contenidoConsumoEnergyActual = document.querySelector("#contenidoConsumoEnergyActual");
-const contenidoConsumoAguaActual = document.querySelector("#contenidoConsumoAguaActual");
+// const contenidoConsumoEnergyActual = document.querySelector("#contenidoConsumoEnergyActual");
+// const contenidoConsumoAguaActual = document.querySelector("#contenidoConsumoAguaActual");
+//
+// const contenidoConsumoEnergyMensual = document.querySelector("#contenidoConsumoEnergyMensual");
+// const contenidoConsumoAguaMensual = document.querySelector("#contenidoConsumoAguaMensual");
+//
+// const graficaSemanalEnergy = document.querySelector("#graficaSemanalEnergy");
+// const graficaSemanalAgua = document.querySelector("#graficaSemanalAgua");
+//
+// const graficaMensualEnergy = document.querySelector("#graficaMensualEnergy");
+// const graficaMensualAgua = document.querySelector("#graficaMensualAgua");
+//
+// const contenidoActual = [contenidoConsumoEnergyActual, contenidoConsumoAguaActual, graficaSemanalEnergy, graficaSemanalAgua];
+// const contenidoMensual = [contenidoConsumoEnergyMensual, contenidoConsumoAguaMensual, graficaMensualEnergy, graficaMensualAgua];
 
-const contenidoConsumoEnergyMensual = document.querySelector("#contenidoConsumoEnergyMensual");
-const contenidoConsumoAguaMensual = document.querySelector("#contenidoConsumoAguaMensual");
-
-const graficaSemanalEnergy = document.querySelector("#graficaSemanalEnergy");
-const graficaSemanalAgua = document.querySelector("#graficaSemanalAgua");
-
-const graficaMensualEnergy = document.querySelector("#graficaMensualEnergy");
-const graficaMensualAgua = document.querySelector("#graficaMensualAgua");
-
-const contenidoActual = [contenidoConsumoEnergyActual, contenidoConsumoAguaActual, graficaSemanalEnergy, graficaSemanalAgua];
-const contenidoMensual = [contenidoConsumoEnergyMensual, contenidoConsumoAguaMensual, graficaMensualEnergy, graficaMensualAgua];
-const aplicarAnimationCambioVisibilidad = (vistaActual, vistaNueva) => {
-    // vistaNueva.forEach((propiedad) => {propiedad.classList.add('aparecer')});
-    //
-    // vistaActual.forEach((propiedad) => {propiedad.classList.remove('aparecer')});
-    // vistaActual.forEach((propiedad) => {propiedad.classList.add('desaparecer')});
-
-    vistaNueva.forEach((contenidoVista) => contenidoVista.style.display = 'inherit');
-
-    // tituloPrincipalVista.classList.add('desaparecer');
-    //
-    // setTimeout(() => {
-    //     vistaActual.style.display = 'none';
-    //     vistaNueva.style.display = 'inherit';
-    //     tituloPrincipalVista.innerHTML = textoTituloVista;
-    //
-    //     vistaNueva.classList.add('aparecer');
-    //     tituloPrincipalVista.classList.remove(('desaparecer'))
-    //     tituloPrincipalVista.classList.add('aparecer');
-    // }, 1900);  // Duración de la animación en milisegundos (1.9 segundo en este caso)
-    //
-    // tituloPrincipalVista.classList.remove(('aparecer'))
-}
-
-// aplicarAnimationCambioVisibilidad(contenidoActual, contenidoMensual);
+// const obtenerElementos = (seleccionador) => document.querySelector(seleccionador);
+//
+// const contenidoActual = [
+//     ...obtenerElementos("#contenidoConsumoEnergyActual").querySelectorAll('h3, span'),
+//     ...obtenerElementos("#contenidoConsumoAguaActual").querySelectorAll('h3, span')
+//     // obtenerElementos("#graficaSemanalEnergy"),
+//     // obtenerElementos("#graficaSemanalAgua")
+// ];
+//
+// console.log(contenidoActual);
+//
+// const contenidoMensual = [
+//     ...obtenerElementos("#contenidoConsumoEnergyMensual").querySelectorAll('h3, span'),
+//     ...obtenerElementos("#contenidoConsumoAguaMensual").querySelectorAll('h3, span')
+//     // obtenerElementos("#graficaMensualEnergy"),
+//     // obtenerElementos("#graficaMensualAgua")
+// ];
+//
+// const aplicarAnimationCambioVisibilidad = (vistaActual, vistaNueva) => {
+//     vistaActual.forEach((contenidoVista) => {contenidoVista.classList.add('desaparecer')});
+//
+//     setTimeout(() => {
+//         vistaActual.forEach((contenidoVista) => contenidoVista.style.display = 'none');
+//         vistaNueva.forEach((contenidoVista) => contenidoVista.style.display = 'inherit');
+//
+//         vistaNueva.forEach((contenidoVista) => {contenidoVista.classList.add('aparecer')});
+//     }, 1900);  // Duración de la animación en milisegundos (1.9 segundo en este caso)
+//
+//     vistaActual.forEach((contenidoVista) => {contenidoVista.classList.remove('aparecer')});
+//     vistaNueva.forEach((contenidoVista) => {contenidoVista.classList.remove('desaparecer')});
+// }
+//
+// const cambiarVisibilidad = () => {
+//     (document.querySelector("#contenidoConsumoEnergyMensual").style.display === 'none')
+//         ? aplicarAnimationCambioVisibilidad(contenidoMensual, contenidoActual)
+//         : aplicarAnimationCambioVisibilidad(contenidoActual, contenidoMensual);
+// }
+//
+// setInterval(cambiarVisibilidad, 8000);  // 8,000 milisegundos = 8 segundos
