00e3eca65a126fefb0444143195f4283bf1f4aa3
Madkodevian <tatianarh97@gmail.com>
2024-03-12 10:17:30 +0000

    Nueva grafica mensual agua energia
    Cambios en las gráficas mensuales de agua y energía

--- src/main/webapp/js/graficas/graficaMensualEnergy.js
+++ src/main/webapp/js/graficas/graficaMensualEnergy.js
@@ -3,58 +3,75 @@ const myChartMensualEnergy = echarts.init(document.getElementById('graficaMensua
 
 // Specify the configuration items and data for the chart
 option = {
-  legend: {},
-  tooltip: {},
-  dataset: {
-    dimensions: ['week', 'Semana_anterior', 'Semana_actual'],
-    source: [
-      { week: 'Lunes', Semana_anterior: 43.3, Semana_actual: 85.8 },
-      { week: 'Martes', Semana_anterior: 83.1, Semana_actual: 73.4 },
-      { week: 'Miércoles', Semana_anterior: 86.4, Semana_actual: 65.2 },
-      { week: 'Jueves', Semana_anterior: 72.4, Semana_actual: 53.9 },
-      { week: 'Viernes', Semana_anterior: 43.3, Semana_actual: 5 },
-      { week: 'Sábado', Semana_anterior: 72.4, Semana_actual: 53.9 },
-      { week: 'Domingo', Semana_anterior: 43.3, Semana_actual: 10 }
-    ]
+  title: {
+    text: 'Energía'
   },
-
-  color: [
-    '#C0C51A',
-    '#7F8206'
-  ],
-
+  tooltip: {
+    trigger: 'axis',
+    axisPointer: {
+      type: 'cross',
+      label: {
+        backgroundColor: '#6a7985'
+      }
+    }
+  },
+  legend: {
+    data: ['Consumo mensual']
+  },
+  toolbox: {
+    feature: {
+      saveAsImage: {}
+    }
+  },
   grid: {
     left: '3%',
     right: '4%',
-    bottom: '25%', // Aumenta el espacio en la parte inferior para hacer espacio para las etiquetas del eje x
+    bottom: '3%',
     containLabel: true
   },
-
-  xAxis: {
-    type: 'category',
-    axisTick: { show: false }, // Oculta las marcas del eje x para que solo se muestren las etiquetas
-    data: ['Lun [4 y 11]', 'Mar [5 y 12]', 'Mie [6 y 13]', 'Jue [7 y 14]', 'Vie [8 y 15]', 'Sab [9 y 16]', 'Dom [10 y 17]'] // Etiquetas personalizadas del eje x
-  },
-  yAxis: {},
-
+  xAxis: [
+    {
+      type: 'category',
+      boundaryGap: false,
+      data: ['Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep',
+      'Oct', 'Nov', 'Dic', 'Ene', 'Feb', 'Mar']
+    }
+  ],
+  yAxis: [
+    {
+      type: 'value'
+    }
+  ],
   series: [
     {
-      type: 'bar',
-      data: [93.3, 83.1, 86.4, 72.4, 43.3, 72.4, 43.3], // Datos de la semana anterior
-      label: { // Configuración de la etiqueta
-        show: true, // Mostrar etiqueta
-        position: 'inside', // Posición de la etiqueta
-        formatter: '{c}' // Formato de la etiqueta, "{c}" representa el valor del dato
-      }
-    },
-    {
-      type: 'bar',
-      data: [85.8, 73.4, 15.2, 53.9, 5, 53.9, 99], // Datos de la semana actual
-      label: { // Configuración de la etiqueta
-        show: true, // Mostrar etiqueta
-        position: 'inside', // Posición de la etiqueta
-        formatter: '{c}' // Formato de la etiqueta, "{c}" representa el valor del dato
-      }
+      name: 'Consumo mensual',
+      type: 'line',
+      stack: 'Total',
+      label: {
+        show: true,
+        position: 'top'
+      },
+
+      //Cambiar el color del interior de la grafica
+      areaStyle: {
+        //Color R/G/B/Alfa
+        color: 'rgba(230, 232, 57, 0.5)'
+      },
+      emphasis: {
+        focus: 'series'
+      },
+
+      //Cambiar el color de la linea de la grafica
+      lineStyle: {
+        color: '#CBCD2A'
+      },
+
+      //Cambiar el color de los circulos de la grafica
+       itemStyle: {
+        color: '#CBCD2A'
+      },
+
+      data: [820, 932, 901, 934, 1290, 1330, 1320, 1000, 1200, 1100, 1000, 1005]
     }
   ]
 };
