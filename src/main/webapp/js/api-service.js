20c0286d3ab38ebf1674bdcb119e4590888b39ed
JordanAlemanSantan-IESElrincon <125272829+JordanAlemanSantan-IESElrincon@users.noreply.github.com>
2024-03-14 13:14:19 +0000

    Añadiendo sección html

--- src/main/webapp/js/api-service.js
+++ src/main/webapp/js/api-service.js
@@ -26,4 +26,4 @@ export async function fetchMonthlyConsumption(waterId, lightId) {
     }
 }
 
-export const tiempoEsperaParaVolverASolicitarDatos = 5000;
+export const tiempoEsperaParaVolverASolicitarDatos = 900000;
