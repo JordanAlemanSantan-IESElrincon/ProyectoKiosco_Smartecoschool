b59b2b0edaa5e393c8f658bf83070a0c95234003
Jordan <jordanmanuelalemansantana@alumno.ieselrincon.es>
2024-03-14 20:22:51 +0000

    Cambios controlVistas.js

--- src/main/webapp/js/controlVistas.js
+++ src/main/webapp/js/controlVistas.js
@@ -1,18 +1,43 @@
 // Tiempo de espera para el cambio automático de vistas en milisegundos
-const tiempoEsperaCambioVista = 10000;
+const tiempoEsperaCambioVista = 4000;
 
 // Selección de elementos HTML por sus ID para manipulación posterior
 const contenidoConsumoEnergyActual = document.querySelector("#contenidoConsumoEnergyActual");
 const contenidoConsumoEnergyMensual = document.querySelector("#contenidoConsumoEnergyMensual");
+const contenidoRetoEnergy = document.querySelector("#contenidoRetoEnergy");
 
 const contenidoConsumoAguaActual = document.querySelector("#contenidoConsumoAguaActual");
 const contenidoConsumoAguaMensual = document.querySelector("#contenidoConsumoAguaMensual");
+const contenidoRetoAgua = document.querySelector("#contenidoRetoAgua");
 
 const graficaSemanalEnergy = document.querySelector("#graficaSemanalEnergy");
 const graficaMensualEnergy = document.querySelector("#graficaMensualEnergy");
+const graficarRetoEnergy = document.querySelector("#graficarRetoEnergy");
 
 const graficaSemanalAgua = document.querySelector("#graficaSemanalAgua");
 const graficaMensualAgua = document.querySelector("#graficaMensualAgua");
+const graficarRetoAgua = document.querySelector("#graficarRetoAgua");
+
+const contenidoTodasLasVistas = [
+    [
+        contenidoConsumoEnergyActual,
+        contenidoConsumoAguaActual,
+        graficaSemanalEnergy,
+        graficaSemanalAgua
+    ],
+    [
+        contenidoConsumoEnergyMensual,
+        contenidoConsumoAguaMensual,
+        graficaMensualEnergy,
+        graficaMensualAgua
+    ],
+    [
+        contenidoRetoEnergy,
+        contenidoRetoAgua,
+        graficarRetoEnergy,
+        graficarRetoAgua
+    ]
+]
 
 //      Las gráficas tienen que inicializarse en un div existente y con el suficiente espacio.
 //      Como serán dos gráficas ocupando el mismo espacio de div, se inicializa las mensuales
@@ -24,6 +49,9 @@ const graficaMensualAgua = document.querySelector("#graficaMensualAgua");
 //      div de graficaMensualAgua → contenidoGraficaConsumoAgua
 graficaMensualEnergy.style.visibility = 'hidden';
 graficaMensualAgua.style.visibility = 'hidden';
+
+graficarRetoEnergy.style.visibility = 'hidden';
+graficarRetoAgua.style.visibility = 'hidden';
 
 // Aplica animación para cambiar la visibilidad de las vistas entre actual y mensual.
 const aplicarAnimationCambioVisibilidad = (vistaActual, vistaNueva) => {
@@ -45,20 +73,34 @@ const aplicarAnimationCambioVisibilidad = (vistaActual, vistaNueva) => {
 }
 
 // Cambia la visibilidad entre actual y mensual según su estado actual.
-const cambiarVisibilidad = (vistaActual, vistaMensual) => {
-    (vistaActual.style.display === 'none')
-        ? aplicarAnimationCambioVisibilidad(vistaMensual, vistaActual)
-        : aplicarAnimationCambioVisibilidad(vistaActual, vistaMensual);
-};
+// const cambiarVisibilidad = (vistaActual, vistaMensual) => {
+//     (vistaActual.style.display === 'none')
+//         ? aplicarAnimationCambioVisibilidad(vistaMensual, vistaActual)
+//         : aplicarAnimationCambioVisibilidad(vistaActual, vistaMensual);
+// };
+
+let recorrerContenidos = 0;
 
 // Cambia la visibilidad total alternando entre vistas de consumo de energía y agua.
-const cambiasVisibilidadTotal = () => {
-    cambiarVisibilidad(contenidoConsumoEnergyActual, contenidoConsumoEnergyMensual)
-    cambiarVisibilidad(contenidoConsumoAguaActual, contenidoConsumoAguaMensual)
+// const cambiasVisibilidadTotal = () => {
+//     aplicarAnimationCambioVisibilidad(contenidoConsumoEnergyActual, contenidoConsumoEnergyMensual)
+//     aplicarAnimationCambioVisibilidad(contenidoConsumoAguaActual, contenidoConsumoAguaMensual)
+//
+//     aplicarAnimationCambioVisibilidad(graficaSemanalEnergy, graficaMensualEnergy);
+//     aplicarAnimationCambioVisibilidad(graficaSemanalAgua, graficaMensualAgua);
+// };
 
-    cambiarVisibilidad(graficaSemanalEnergy, graficaMensualEnergy);
-    cambiarVisibilidad(graficaSemanalAgua, graficaMensualAgua);
+const cambiasVisibilidadTotal = () => {
+    aplicarAnimationCambioVisibilidad(contenidoTodasLasVistas[recorrerContenidos][0], contenidoTodasLasVistas[(recorrerContenidos + 1) % 3][0])
+    aplicarAnimationCambioVisibilidad(contenidoTodasLasVistas[recorrerContenidos][1], contenidoTodasLasVistas[(recorrerContenidos + 1) % 3][1])
+
+    aplicarAnimationCambioVisibilidad(contenidoTodasLasVistas[recorrerContenidos][2], contenidoTodasLasVistas[(recorrerContenidos + 1) % 3][2]);
+    aplicarAnimationCambioVisibilidad(contenidoTodasLasVistas[recorrerContenidos][3], contenidoTodasLasVistas[(recorrerContenidos + 1) % 3][3]);
+
+    recorrerContenidos = (recorrerContenidos + 1) % 3;
+    console.log("recorrerContenidos", recorrerContenidos);
 };
+
 
 // Establece la ejecución de la función cada cierto tiempo definido por 'tiempoEsperaCambioVista'.
 setInterval(() => cambiasVisibilidadTotal(), tiempoEsperaCambioVista);
